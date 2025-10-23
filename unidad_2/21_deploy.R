22974b00b00b9ace5f02ca566773d55acf74689c
David Andres Quintela Saavedra <david.a.quintela@gmail.com>
2025-10-23 16:03:27 -0400

    agregue los datos de la direccion para que haga deploy en la pagina correctamente

--- unidad_2/21_deploy.R
+++ unidad_2/21_deploy.R
@@ -1,11 +1,11 @@
 # install.packages("rsconnect")
 library(rsconnect)
  
-rsconnect::setAccountInfo(name=' ',
-                          token=' ',
-                          secret=' ')
+rsconnect::setAccountInfo(name='dqdatasci',
+                          token='55BD8E47CE6658D123F9A1528A45110A',
+                          secret='7SogJUYNK6UfEyrW1OcQssk+LdGGLMp8/TemclU/')
  
-rsconnect::deployApp(appDir = "C://..../21_Shiny",
+rsconnect::deployApp(appDir = "C://Users/HP/Projects/est_4/unidad_2/21_shiny/",
                      appName = "21_myapp")
  
  
