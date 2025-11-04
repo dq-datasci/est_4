3eae053e6b911f613b0ff01b03a5244922ac9838
David Andres Quintela Saavedra <david.a.quintela@gmail.com>
2025-11-04 16:19:47 -0400

    Actualización a la versión final del profesor (v3.0)

--- unidad_2/22_shiny/R/stats.R
+++ unidad_2/22_shiny/R/stats.R
@@ -1,24 +1,14 @@
-# Función para generar la población (usa la primera semilla)
-generar_poblacion <- function(seed_poblacion, n, p) {
-  set.seed(seed_poblacion)
+generar_poblacion <- function(n, p) {
   rbinom(n, 1, p)
 }
+# x <- generar_poblacion(123,1000,0.3)
 
-# x <- generar_poblacion(123, 1000, 0.3)
-
-# Función para tomar la muestra (usa la segunda semilla para el sampling)
-tomar_muestra <- function(poblacion, n_muestra, seed_muestra) {
-  # Establecer la semilla de la muestra para asegurar que el proceso de 'sample' sea reproducible
-  set.seed(seed_muestra)
+tomar_muestra <- function(poblacion, n_muestra) {
   sample(poblacion, n_muestra, replace = FALSE)
 }
+# y <- tomar_muestra(x,100)
 
-# tomar_muestra(c(1, 0, 0, 1, 0, 1, 0), 2)
-# y <- tomar_muestra(x, 100)
-
-# Función para calcular la proporción muestral
 calcular_proporcion <- function(muestra) {
   sum(muestra) / length(muestra)
 }
-
-# calcular_proporcion(y)
+#calcular_proporcion(y) 
