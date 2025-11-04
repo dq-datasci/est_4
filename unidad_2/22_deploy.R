a0e9805e81312b3bbdea0355f271be1b51744c90
David Andres Quintela Saavedra <david.a.quintela@gmail.com>
2025-11-04 15:40:29 -0400

    configure el archivo de deploy para que pueda usar la pagina en la web

---
+++ unidad_2/22_deploy.R
@@ -0,0 +1,12 @@
+# install.packages("rsconnect")
+library(rsconnect)
+
+rsconnect::setAccountInfo(name='dqdatasci',
+                          token='55BD8E47CE6658D123F9A1528A45110A',
+                          secret='7SogJUYNK6UfEyrW1OcQssk+LdGGLMp8/TemclU/')
+
+rsconnect::deployApp(appDir = "C://Users/HP/Projects/est_4/unidad_2/22_shiny/",
+                     appName = "22_myapp")
+
+
+rsconnect::showLogs(appName = "22_myapp")
